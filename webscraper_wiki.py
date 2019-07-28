05e57c8e2843d5af69569c5aec6461bc9a6110ee
achitta <achitta@umich.edu>
2019-07-27 23:41:37 -0400

    Fixed bugs with Wikepedia Scraper

--- webscraper_wiki.py
+++ webscraper_wiki.py
@@ -3,16 +3,16 @@ import requests
 import json
 
 def scrape_degrees(start, end, count, count_fixed):
-    print(start)
-    print(count)
+    # print(start)
+    # print(count)
     if(len(start) < 3) :
-        return
-
+        return False
+    found_flag = False
     flag = True
     if(count == 1) :
         flag = False
     if(count == 0) :
-        return
+        return False
 
     url = 'https://en.wikipedia.org/wiki/' + start
     response = requests.get(url, timeout=5)
@@ -25,18 +25,26 @@ def scrape_degrees(start, end, count, count_fixed):
             if (ord(link.text[0:1]) >= 65 and ord(link.text[0:1]) <= 90) or (ord(link.text[0:1]) >= 97 and ord(link.text[0:1]) <= 122) :
                 hyperlinks.append(link.text)
                 limit = limit + 1
-                if(link.text == end) :
+                if(link.text.lower() == end.lower()) :
                     print("Path Found!")
                     print(str(count_fixed-count) + " degrees away!")
-                    exit(0)
+                    print(end)
+                    print(start)
+                    return True
+                    # exit(0)
                 if(flag) :
-                    scrape_degrees(link.text, end, count - 1, count_fixed)
-                    print("Back to " + start)
+                    found_flag = scrape_degrees(link.text, end, count - 1, count_fixed)
+                    if(found_flag) :
+                        print(start)
+                        return True
+                    # print( "Back to " + start)
                 if(limit > 50) :
-                    print("Reached Limit")
-                    return
-    print("Length of Array for " + start + ": " + str(len(hyperlinks))) 
-    print('\n')
+                    # print("Reached Limit")
+                    return False
+    return False
+    # print("Length of Array for " + start + ": " + str(len(hyperlinks))) 
+    # print('\n')
+    
         
     with open('wikiData.json', 'w') as fp:
         json.dump(hyperlinks, fp, indent=3)
@@ -55,7 +63,7 @@ def scrape_func(start) :
         if(len(link.text) < 12 and len(link.text) > 2) : 
             if (ord(link.text[0:1]) >= 65 and ord(link.text[0:1]) <= 90) or (ord(link.text[0:1]) >= 97 and ord(link.text[0:1]) <= 122) :
                 hyperlinks.append(link.text)
-    print("Length of Array for " + start + ": " + str(len(hyperlinks))) 
+    # print("Length of Array for " + start + ": " + str(len(hyperlinks))) 
     
     
     with open('wikiData.json', 'w') as fp:
@@ -69,4 +77,3 @@ ending_topic = input("Enter Ending Topic: ")
 degrees = int(input("Enter number of degrees: "))
 
 scrape_degrees(starting_topic, ending_topic, degrees, degrees+1)
-# scrape_func("Monkey")
